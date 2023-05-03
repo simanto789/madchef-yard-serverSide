b4e07fbca050578858c590cc61ca61901acd07f7
simanto789 <hazrat.ali789@gmail.com>
2023-05-03 21:45:52 +0600

    pass madchef data to the client side

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+const chefdata = require('./data/chefdata.json');
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('madchef is running')
  
 });
+
+app.get('/chefdata', (req, res) =>{
+    res.send(chefdata);
+})
 
 app.listen(port, ()=>{
     console.log(`madchef is running on port: ${port}`)
