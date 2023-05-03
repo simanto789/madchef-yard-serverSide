86f0d41a85b2c6aecaca6baee220de01ff09ea89
simanto789 <hazrat.ali789@gmail.com>
2023-05-03 20:52:53 +0600

    run the server side and update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.send('madchef is running')
+ 
+});
+
+app.listen(port, ()=>{
+    console.log(`madchef is running on port: ${port}`)
+})
